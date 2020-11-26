26174e7461d4c666aefb1aeb0bdf4d3a948c112e
project-Lee <ddw099@naver.com>
2020-11-26 22:20:27 +0900

    수정중

--- HiBabySQL.sql
+++ HiBabySQL.sql
@@ -17,13 +17,6 @@ CREATE TABLE t_user(
 );
 SELECT * FROM t_user;
 
-SELECT * FROM t_board;
-
-UPDATE t_user
-SET myPoint = 20000,
-myCash = 1000
-WHERE i_user = 2;
-
 
 
 CREATE TABLE t_board(
@@ -37,37 +30,22 @@ CREATE TABLE t_board(
 );
 SELECT * FROM t_board;
 
-INSERT INTO t_board (title, ctnt, i_user) VALUES ('11111','1111111111',1);
-INSERT INTO t_board (title, ctnt, i_user) VALUES ('222222','22222',1);
-INSERT INTO t_board (title, ctnt, i_user) VALUES ('33333','3333333',1);
-INSERT INTO t_board (title, ctnt, i_user) VALUES ('44444','44444444',2);
-
-
 
 CREATE TABLE t_myPoint(
 	i_point INT UNSIGNED AUTO_INCREMENT,
 	i_user INT,
-	myPoint INT(8), -- 기존포인트
-	myCash INT(8),  -- 환급 캐시백
-	myPointNow INT(8),	-- 환급후 남은 포인트
+	myPoint INT(8),
+	myCash INT(8),
+	myPointNow INT(8),
 	r_dt DATETIME DEFAULT NOW(),
 	PRIMARY KEY(i_point, i_user),
 	FOREIGN KEY(i_user) REFERENCES t_user(i_user)
 );
 
-DROP TABLE t_myPoint;
-SELECT * FROM t_myPoint;
-
--- 포인트 환전내역 나타낼때 사용할 쿼리문임
-SELECT A.i_point, A.myPoint, A.myCash, A.myPointNow, A.r_dt, 
-A.i_point, A.i_user, B.i_user, B.nick
-FROM t_myPoint A
-
-LEFT JOIN t_user B
-ON A.i_user = B.i_user
-WHERE A.i_user = 2;
-
-
-
-
+SELECT A.i_point, A.myPoint, A.myCash, A.myPointNow, A.i_point, A.i_user,
+        B.i_user, B.nick, A.r_dt FROM t_myPoint A
+		LEFT JOIN t_user B
+		ON A.i_user = B.i_user
+		WHERE A.i_user = 1
+		ORDER BY A.r_dt DESC;
 
