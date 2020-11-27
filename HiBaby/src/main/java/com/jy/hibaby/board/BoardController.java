7aaaf575c860c60103a30479e4af30246e2c0224
monkeymagic92 <ddw0099@naver.com>
2020-11-27 23:57:42 +0900

    글등록 이미지 작업중

--- HiBaby/src/main/java/com/jy/hibaby/board/BoardController.java
+++ HiBaby/src/main/java/com/jy/hibaby/board/BoardController.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jy.hibaby.ViewRef;
 import com.jy.hibaby.board.model.BoardDMI;
+import com.jy.hibaby.board.model.BoardPARAM;
 import com.jy.hibaby.user.model.UserPARAM;
 
 @Controller
@@ -26,7 +27,7 @@ public class BoardController {
 	
 	// 게시글 전체 목록
 	@RequestMapping(value="/list", method = RequestMethod.GET)
-	public String boardList(Model model) {
+	public String boardList(Model model, BoardPARAM param) {
 		List<BoardDMI> list = new ArrayList<BoardDMI>();
 		
 		list = service.selBoard();
@@ -50,11 +51,33 @@ public class BoardController {
 	public String insBoard(Model model, BoardDMI param, MultipartHttpServletRequest mReq,
 			HttpSession hs, RedirectAttributes ra, UserPARAM vo) {
 		
+		int boardResult = Integer.parseInt(mReq.getParameter("boardResult"));
+		System.out.println("updResult = " + boardResult);
 		System.out.println("param user : " + param.getI_user());
 		int result = service.insBoard(param, mReq, hs);
-		model.addAttribute("view", ViewRef.BOARD_INSBOARD);
-		return "redirect:/" + ViewRef.BOARD_LIST;
-	}
-	
-	
+		if(boardResult == 1) { // 글등록
+			try {
+				if(result == 1) {
+					// DETAIL.GET 에서 index/main, mypage, SalaReg 모두다 request.getParameter()로 받게하기위해
+					int i_board = (int)hs.getAttribute("i_board");
+					ra.addAttribute("i_board",i_board);
+					return "redirect:/" + ViewRef.BOARD_LIST;
+					
+				} else if(result == 2){
+					ra.addFlashAttribute("ImageFail","입력되지 않은 항목 이 있습니다");
+					return "redirect:/" + ViewRef.BOARD_INSBOARD;
+					
+				} else {
+					ra.addFlashAttribute("ImageFail","서버에러 다시 다시 시도해주세요");
+					return "redirect:/" + ViewRef.BOARD_INSBOARD;
+				}
+			} catch(Exception e) {
+				ra.addFlashAttribute("serverErr","서버에러 다시 시도해주세요");
+				return "redirect:/" + ViewRef.BOARD_INSBOARD;
+			}
+		} else { // 글수정
+			return "";
+		}
+		
+	}		
 }
--- HiBaby/src/main/webapp/WEB-INF/views/board/insBoard.jsp
+++ HiBaby/src/main/webapp/WEB-INF/views/board/insBoard.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +13,7 @@
 <body>
 	<form id="frm" action="/board/insBoard" method="post" enctype="multipart/form-data" onsubmit="return chk()">
 		파일등록 :
-		<input type="file" name="images"  id="mfile" multiple accept="image/png, image/jpeg, image/jpg">
+		<input type="file" name="images" id="mfile" multiple accept="image/png, image/jpeg, image/jpg">
 		<br><br>
 		제목 :
 		<input type="text" name="title">
@@ -22,7 +23,30 @@
 			
 		</textarea>
 		<input type="hidden" name="i_user" value="${loginUser.i_user}">
+		<c:if test="${data.i_board == null }">
+        	<input type="hidden" name="boardResult" value="1">
+        </c:if>
+        <c:if test="${data.i_board != null }">
+        	<input type="hidden" name="boardResult" value="2">
+        	<input type="hidden" name="i_board" value="${data.i_board }"> 
+        </c:if>
 		<input type="submit" value="글등록">
 	</form>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+if(${ImageFail != null}) {
+	alert('${ImageFail}')
+} 
+
+if(${serverErr != null}) {
+	alert('${serverErr}')
+}
+
+// 이미지 5장까지 업로드가능하게 막기 
+
+function chk() {
+	
+}
+</script>
 </body>
 </html>
